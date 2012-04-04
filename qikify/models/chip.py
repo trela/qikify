526598a8f3527dd791d9efae260587b74d145bf4
Abhishek Basu <sendtoabhishek.bose@gmail.com>
2012-04-04 13:09:35 -0500

    KNN Implementation

--- qikify/models/chip.py
+++ qikify/models/chip.py
@@ -1,26 +1,31 @@
 
 class Chip(object):
     import pandas
-    def __init__(self, chip_dict, LCT_prefix = ''):
+    def __init__(self, chip_dict=None, LCT=None, HCT=None, gnd=None, LCT_prefix=''):
         """Expects a dictionary of chip data. A prefix indicating which 
         parameters are low-cost test data is also expected."""
 
-        self.LCT = {}
-        self.HCT = {}
-        self.gnd=0
-        try:
-            self.id = chip_dict['WAFER_ID'] + ':' + chip_dict['XY']
-        except:
-            self.id = 'unknown_chip_id'
+        if chip_dict is None:
+            self.LCT = LCT
+            self.HCT = HCT
+            self.gnd = gnd
 
-        import pdb; pdb.set_trace()
-        
+        else:
+            
+            self.LCT = {}
+            self.HCT = {}
+            self.gnd=0
+            try:
+                self.id = chip_dict['WAFER_ID'] + ':' + chip_dict['XY']
+            except:
+                self.id = 'unknown_chip_id'
 
-        for k in chip_dict:
-            if k=="gnd":
-                self.gnd=chip_dict[k]
-            elif k.startswith(LCT_prefix):
-                self.LCT[k] = chip_dict[k]
-            else:
-                self.HCT[k] = chip_dict[k]
+
+            for k in chip_dict:
+                if k=="gnd":
+                    self.gnd=chip_dict[k]
+                elif k.startswith(LCT_prefix):
+                    self.LCT[k] = chip_dict[k]
+                else:
+                    self.HCT[k] = chip_dict[k]
 
--- qikify/recipes/basic_ML_testing.py
+++ qikify/recipes/basic_ML_testing.py
@@ -2,11 +2,14 @@
 import zmq
 import msgpack
 import time
+from qikify.models.KNNImplementation import KNNImplementation
+from qikify.models import Chip
 
 class BasicMLTesting(object):
     def __init__(self):
         self.chip_buffer = []
         self.recv_count = 0
+        self.knn = KNNImplementation()
 
     def run(self, port = 5570):
         self.port = port
@@ -20,11 +23,22 @@ class BasicMLTesting(object):
         try:
             while True:
                 print 'Requesting chip instance ...',
-                self.socket.send('REQ:send_LCT')
 
+                self.socket.send('REQ:send_LCT')
                 unpacker.feed(self.socket.recv())
-                chip = unpacker.unpack()
-                if chip is not None:
+                chip_lct = unpacker.unpack()
+                
+                # assert self.socket.recv() == 'RES:ack', \
+                #         'Error: invalid ack from ATE simulator'
+                # print chip_lct
+                # time.sleep(10)
+                self.socket.send('REQ:send_gnd')
+                unpacker.feed(self.socket.recv())
+                chip_gnd =unpacker.unpack()
+
+                chip = Chip(LCT=chip_lct, gnd=chip_gnd)
+
+                if self.recv_count<=1000 and chip is not None:
                     self.recv_count += 1
                     self.chip_buffer.append(chip)
                     print 'received chip #', self.recv_count
@@ -34,8 +48,11 @@ class BasicMLTesting(object):
                     assert self.socket.recv() == 'RES:ack', \
                         'Error: invalid ack from ATE simulator'
                 if self.recv_count==1000:
-                    recv_count=0
+                    #recv_count=0
                     print "Training Algorithm will run on these 1000 chips"
+                    self.knn.trainmodel(self.chip_buffer)
+                if self.recv_count > 1000:
+                    self.knn.predict(chip)
 
         except KeyboardInterrupt:
             print '\nterminating basic ML testing.'
